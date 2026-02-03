a7304efaafc37c34ed0572a0f986a9c43bce7ca3
shekinahwebdev <patriciashilohkanneh12@gmail.com>
2026-02-03 03:27:29 +0000

    Comment out unused state and imports in ImageUpload and Dashboard components

--- src/admin/pages/Dashboard.tsx
+++ src/admin/pages/Dashboard.tsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
+// import { useState } from "react";
 import MainBoard from "../components/MainBoard";
-import PageEditor from "./PageEditor";
+// import PageEditor from "./PageEditor";
 
 const Dashboard = () => {
-  const [currentRoute, setCurrentRoute] = useState("/admin");
+  // const [currentRoute, setCurrentRoute] = useState("/admin");
 
   return (
     <section className="md:px-0 flex flex-row text-white relative">
-      {currentRoute == "/admin" ? (
+      {/* {currentRoute == "/admin" ? (
         <MainBoard />
       ) : (
         <PageEditor pageId={currentRoute} />
-      )}
+      )} */}
+      <MainBoard />
     </section>
   );
 };
